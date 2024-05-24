d37b77e57c6faa06167d44616044d8a652ecb42b
Audey Orcajada <audeyorcajada23@gmail.com>
2024-05-24 22:07:25 +0800

    tenth

--- tests.py
+++ tests.py
@@ -7,7 +7,6 @@ class MyAppTests(unittest.TestCase):
     def setUp(self):
         app.config["TESTING"] = True
         self.app = app.test_client()
-
         warnings.simplefilter("ignore", category=DeprecationWarning)
 
     def test_index_page(self):
@@ -18,12 +17,12 @@ class MyAppTests(unittest.TestCase):
     def test_getactors(self):
         response = self.app.get("/employee")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue("PENELOPE" in response.data.decode())
+        self.assertTrue("James" in response.data.decode())
 
     def test_getactors_by_id(self):
-        response = self.app.get("/employee/88")
+        response = self.app.get("/employee/888665555")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue("PESCI" in response.data.decode())
+        self.assertTrue("James" in response.data.decode())
 
 
 if __name__ == "__main__":
